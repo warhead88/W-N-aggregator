5ebc661f7d148ea2ce0e5e91a9780b65f0b2cbb2
w88 <krippach147@gmail.com>
2025-09-28 20:28:20 +0500

    Added database (sqlite)

---
+++ db/db.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from contextlib import asynccontextmanager
+
+from tables import Base
+
+DSATABASE_URL = "sqlite+aiosqlite:///./main.db"
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True,
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@asynccontextmanager
+async def get_async_sesion():
+    try:
+        yield session
+        await session.commit()
+    except:
+        await session.rollback()
+        raise
